b2dd8599a90642eb782e0934e90aafd645d8c8d2
mitropnikos <128041907+mitropnikos@users.noreply.github.com>
2023-03-28 10:43:49 +0300

    Update sqlqueries.py

--- PostgreSQL Upsert/sqlqueries.py
+++ PostgreSQL Upsert/sqlqueries.py
@@ -11,14 +11,14 @@ create table citus_staging_tbl
 """
 
 query3 = """
-insert into hdfs_citus_staging_tbl
+insert into hdfs_citus_pridge_connection
 select * from hdfs_staging_table 
 """
 
 query4 = """
 insert into citus_prd_tbl
 select *
-from hdfs_citus_staging_tbl
+from citus_staging_tbl
 on conflict (local_date.....)
 do 
 	update set 	
