a39e4ac04f023a7ef1d430934dbf9c3629463708
mitropnikos <128041907+mitropnikos@users.noreply.github.com>
2023-03-28 10:35:55 +0300

    Create sqlqueries.py

---
+++ PostgreSQL Upsert/sqlqueries.py
@@ -0,0 +1,26 @@
+query1 = """
+query on hdfs
+"""
+
+query2 = """
+create table hdfs_staging_tbl 
+(
+....
+)
+"""
+
+query3 = """
+insert into hdfs_citus_staging_tbl
+select * from hdfs_staging_tbl 
+"""
+
+query4 = """
+insert into citus_prd_tbl
+select *
+from hdfs_citus_staging_tbl
+on conflict (local_date.....)
+do 
+	update set 	
+				subscribed = EXCLUDED.subscribed,
+        ......
+"""
