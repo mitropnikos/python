87f66300dfe2b012aeed47f3d36493482d37abda
mitropnikos <128041907+mitropnikos@users.noreply.github.com>
2023-05-08 11:17:39 +0300

    Create killing_block_partitions.py

---
+++ killing_block_partitions.py
@@ -0,0 +1,23 @@
+import psycopg2
+from datetime import datetime
+
+if __name__ == '__main__':
+        print "{", datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "} Starting killing"
+        conn = psycopg2.connect(
+                "dbname=edb user=enterprisedb host=localhost port=5444")
+        cursor = conn.cursor()
+        cursor.execute( "select pid from"
+                        "(select *,((EXTRACT(epoch FROM (SELECT (NOW() - query_start))))::int) as sec "
+                        "from pg_Stat_activity where state='active' and  waiting=true "
+                        "and query like '%ADD_PARTITION%')where sec>10; "
+                      )
+        pids = cursor.fetchall()
+        num_of_processes_killed=0
+        for pid_info in pids:
+                pid = pid_info[0]
+                #user = pid_info[1].strip()
+                print "[", datetime.now().strftime('%Y-%m-%d %H:%M:%S') , "] Killing process with pid:", pid
+                cursor.execute('select pg_terminate_backend ({0});'.format(pid))
+                num_of_processes_killed+=1
+        print "{", datetime.now().strftime('%Y-%m-%d %H:%M:%S') ,"} Killed #", num_of_processes_killed, "processes"
+        print "{", datetime.now().strftime('%Y-%m-%d %H:%M:%S') ,"} Stop killing"
