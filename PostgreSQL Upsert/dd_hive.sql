dc2e53127e79e1c9cb558eb235cf0c6a87cd0fed
mitropnikos <128041907+mitropnikos@users.noreply.github.com>
2023-11-11 15:51:55 +0200

    Delete PostgreSQL Upsert/dd_hive.sql

--- PostgreSQL Upsert/dd_hive.sql
+++
@@ -1,7 +0,0 @@
--- Bridge Connecton Hive - SQL
-CREATE TABLE hdfs_citus_pridge_connection
-USING org.apache.spark.sql.jdbc
-OPTIONS (
-	url "jdbc:postgresql://citus01....:{}port/citus?user=citus&password={pass}",
-	dbtable="citus_staging_tbl "
-)
